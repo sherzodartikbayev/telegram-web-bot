965f8a05459c961ab0b80692af8ad7b087112810
Sherzod <sherzadartikbayev@gmail.com>
2024-09-07 22:13:53 +0500

    API and Main Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,101 @@
-import { getData } from "./constants/db"
-import { Card } from "./components"
-import "./App.css"
-import Cart from "./components/cart/cart"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+import Card from './components/card/card';
+import Cart from './components/cart/cart';
+import { getData } from './constants/db';
 
-const courses = getData()
+const courses = getData();
 
 const telegram = window.Telegram.WebApp;
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    telegram.ready()
-  })
+    telegram.ready();
+  });
 
-  const onAddItem = (item) => {
-    const existItem = cartItems.find(c => c.id === item.id)
+  const onAddItem = item => {
+    const existItem = cartItems.find(c => c.id == item.id);
 
     if (existItem) {
-      const newData = cartItems.map(c => c.id === item.id ? { ...existItem, quantity: existItem.quantity + 1 } : c)
-      setCartItems(newData)
+      const newData = cartItems.map(c =>
+        c.id == item.id
+          ? { ...existItem, quantity: existItem.quantity + 1 }
+          : c
+      );
+      setCartItems(newData);
     } else {
-      const newData = [...cartItems, { ...item, quantity: 1 }]
-      setCartItems(newData)
+      const newData = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(newData);
     }
-  }
+  };
 
-  const onRemoveItem = (item) => {
-    const existItem = cartItems.find(c => c.id === item.id)
+  const onRemoveItem = item => {
+    const existItem = cartItems.find(c => c.id == item.id);
 
     if (existItem.quantity === 1) {
-      const newData = cartItems.filter(c => c.id !== existItem.id)
-      setCartItems(newData)
+      const newData = cartItems.filter(c => c.id !== existItem.id);
+      setCartItems(newData);
     } else {
-      const newDate = cartItems.map(c => c.id === existItem.id ? { ...existItem, quantity: existItem.quantity - 1 } : c)
-      setCartItems(newDate)
+      const newData = cartItems.map(c =>
+        c.id === existItem.id
+          ? { ...existItem, quantity: existItem.quantity - 1 }
+          : c
+      );
+      setCartItems(newData);
     }
-  }
+  };
 
-  const onCheckOut = () => {
-    telegram.MainButton.text = "Sotib olish :)"
-    telegram.MainButton.show()
-  }
+  const onCheckout = () => {
+    telegram.MainButton.text = 'Sotib olish :)';
+    telegram.MainButton.show();
+  };
+
+  const onSendData = useCallback(() => {
+    const queryID = telegram.initDataUnsafe?.query_id;
+
+    if (queryID) {
+      fetch(
+        'https://telegramwebapibot-b671371abfbb.herokuapp.com/web-data',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            products: cartItems,
+            queryID: queryID,
+          }),
+        }
+      );
+    } else {
+      telegram.sendData(JSON.stringify(cartItems));
+    }
+  }, [cartItems]);
+
+  useEffect(() => {
+    telegram.onEvent('mainButtonClicked', onSendData);
+
+    return () => telegram.offEvent('mainButtonClicked', onSendData);
+  }, [onSendData]);
 
   return (
     <>
-      <h1 className="heading">Sammi kurslari</h1>
-      <Cart cartItems={cartItems} onCheckOut={onCheckOut} />
-      <div className="cards__container">
-        {courses.map((course) => (
-          <Card key={course.id} course={course} onAddItem={onAddItem} onRemoveItem={onRemoveItem} />
+      <h1 className='heading'>Sammi kurslar</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
+      <div className='cards__container'>
+        {courses.map(course => (
+          <Card
+            key={course.id}
+            course={course}
+            onAddItem={onAddItem}
+            onRemoveItem={onRemoveItem}
+          />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
